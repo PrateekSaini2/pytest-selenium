3ad74d8d558557aa73af2041654565f1cf6b0d00
PrateekSaini2 <Prateek.Saini2@globallogic.com>
2023-08-01 10:08:35 +0100

    trying print again

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,8 +5,6 @@ from tests.test_base import TestBase
 
 class TestLogin(TestBase):
     locked_user = os.environ.get("SAUCE_LOCKED_USER_NAME", "locked_out_user")
-    print(os.getenv("SAUCE_PERFORMANCE_USER_NAME"))
-    print(os.environ.get("SAUCE_PERFORMANCE_USER_NAME"))
     
     @pytest.mark.parametrize('username, password', [
         ('standard_user', 'secret_sauce'), 
@@ -15,6 +13,8 @@ class TestLogin(TestBase):
         ('performance_glitch_user', 'secret_sauce')]
         )
     def test_login(self, username, password):
+        print(os.getenv("SAUCE_PERFORMANCE_USER_NAME"))
+        print(os.environ.get("SAUCE_PERFORMANCE_USER_NAME"))
         self.driver.find_element(By.ID, "user-name").send_keys(username)
         self.driver.find_element(By.ID, "password").send_keys(password)
         self.driver.find_element(By.ID, "login-button").click()
